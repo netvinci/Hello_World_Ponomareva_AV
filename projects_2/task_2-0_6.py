6936fd63d5b99fd9a72a996f655c24dbf78844ac
netvinci <navernoevinci@gmail.com>
2026-02-12 16:16:21 +0300

    Delete projects_2 directory

--- projects_2/task_2-0_6.py
+++
@@ -1,3 +0,0 @@
-f=open("output.txt", "w", encoding="utf-8")
-print("результаты работы", file=f)
-f.close()
--- projects_2/task_2_1/task_2-1_2.py
+++
@@ -1,4 +0,0 @@
-python_version = 3.14
-
-print("Language: Python", end="\n\t")
-print(f"Version: {python_version}")
